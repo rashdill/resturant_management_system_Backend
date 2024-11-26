475d7fe65ffd6bde07a0f2b716e634db5cfdb854
tharushikaBasnayaka <tharushikabasnayaka1@gmail.com>
2024-11-25 23:04:58 -0500

    Registration

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.config.Encode;
+import com.example.demo.dto.userDTO;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+    Encode encode=new Encode();
+
+//    mailController mail=new mailController();
+
+    @PostMapping("/saveUser")
+    public userDTO saveUser(@RequestBody userDTO userdto)
+    {
+            return userService.saveUSer(userdto);
+    }
+
+
+}
